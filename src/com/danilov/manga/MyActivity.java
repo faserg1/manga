9fa3b34843245852c639423e3db732233aceeb5f
Semen Danilov <samvimes@yandex.ru>
2014-05-15 03:36:06 +0400

    init commit

---
+++ src/com/danilov/manga/MyActivity.java
@@ -0,0 +1,21 @@
+package com.danilov.manga;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ListView list = (ListView) findViewById(R.id.list);
+        String[] array = {"Episodes: 308", "Episodes: 308", "Episodes: 308", "Episodes: 308"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.manga_list_item, R.id.manga_quantity,array);
+        list.setAdapter(adapter);
+    }
+}
