2044ee8982a445306552859bff1dc96fcf027b9c
Semen Danilov <samvimes@yandex.ru>
2014-05-16 00:56:04 +0400

    added imageview for manga

--- src/com/danilov/manga/MyActivity.java
+++ src/com/danilov/manga/MyActivity.java
@@ -1,9 +1,12 @@
 package com.danilov.manga;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.danilov.manga.test.TouchImageViewActivityTest;
 
 public class MyActivity extends Activity {
     /**
@@ -18,4 +21,10 @@ public class MyActivity extends Activity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.manga_list_item, R.id.manga_quantity,array);
         list.setAdapter(adapter);
     }
+
+    public void firstTest(View view) {
+        Intent intent = new Intent(this, TouchImageViewActivityTest.class);
+        startActivity(intent);
+    }
+
 }
---
+++ src/com/danilov/manga/test/TouchImageViewActivityTest.java
@@ -0,0 +1,17 @@
+package com.danilov.manga.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.danilov.manga.R;
+
+/**
+ * Created by Semyon Danilov on 16.05.2014.
+ */
+public class TouchImageViewActivityTest extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_touch_imageview_activity);
+    }
+}
