60a1380c898c3a31aa76aeca8f6c6aea62654ad3
Semyon Danilov <samvimes@yandex.ru>
2014-06-20 09:09:22 +0400

    started writing my adapter

---
+++ src/com/danilov/manga/test/MangaViewTestActivity.java
@@ -0,0 +1,19 @@
+package com.danilov.manga.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import com.danilov.manga.R;
+
+/**
+ * Created by Semyon Danilov on 20.06.2014.
+ */
+public class MangaViewTestActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_manga_view_activity);
+        ViewPager viewPager;
+    }
+
+}
