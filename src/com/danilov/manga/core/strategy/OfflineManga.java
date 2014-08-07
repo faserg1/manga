aa9dd074817285e3feb60ed6d24bfd1f04463df4
Semen Danilov <samvimes@yandex.ru>
2014-08-07 09:20:29 +0400

    commityou

--- src/com/danilov/manga/core/strategy/OfflineManga.java
+++ src/com/danilov/manga/core/strategy/OfflineManga.java
@@ -1,11 +1,21 @@
 package com.danilov.manga.core.strategy;
 
 import com.danilov.manga.core.interfaces.MangaShowStrategy;
+import com.danilov.manga.core.model.LocalManga;
+import com.danilov.manga.core.view.MangaImageSwitcher;
 
 /**
  * Created by Semyon Danilov on 21.06.2014.
  */
 public class OfflineManga implements MangaShowStrategy {
+
+    private LocalManga manga;
+    private MangaImageSwitcher mangaImageSwitcher;
+
+    public OfflineManga(final LocalManga manga, final MangaImageSwitcher mangaImageSwitcher) {
+        this.manga = manga;
+        this.mangaImageSwitcher = mangaImageSwitcher;
+    }
 
     @Override
     public void showImage(final int i) {
