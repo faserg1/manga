5987cab9683919dbf055ecd2326b6764785fc1e2
Semyon Danilov <samvimes@yandex.ru>
2015-02-23 23:35:15 +0300

    various fixes

--- app/src/main/java/com/danilov/mangareader/fragment/SettingsFragment.java
+++ app/src/main/java/com/danilov/mangareader/fragment/SettingsFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.danilov.mangareader.R;
@@ -22,6 +24,7 @@ public class SettingsFragment extends BaseFragment {
 
     private EditText downloadPath;
     private Button selectPath;
+    private CheckBox disableAds;
 
     private ApplicationSettings settings;
 
@@ -39,16 +42,26 @@ public class SettingsFragment extends BaseFragment {
     public void onActivityCreated(final Bundle savedInstanceState) {
         downloadPath = findViewById(R.id.download_path);
         settings = ApplicationSettings.get(getActivity());
-
+        disableAds = findViewById(R.id.disable_ads);
         final String path = settings.getMangaDownloadBasePath();
 
         downloadPath.setText(path);
-        ((Button) findViewById(R.id.select_folder)).setOnClickListener(new View.OnClickListener() {
+        selectPath = findViewById(R.id.select_folder);
+        selectPath.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 Intent intent = new Intent(getActivity(), FolderPickerActivity.class);
                 intent.putExtra(FolderPickerActivity.FOLDER_KEY, path);
                 startActivityForResult(intent, FOLDER_PICKER_REQUEST);
+            }
+        });
+        disableAds.setChecked(settings.isShowAdvertisement());
+        disableAds.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(final CompoundButton compoundButton, final boolean b) {
+                settings.setShowAdvertisement(b);
+                settings.setFirstLaunch(false);
+                settings.update(getActivity());
             }
         });
         super.onActivityCreated(savedInstanceState);
