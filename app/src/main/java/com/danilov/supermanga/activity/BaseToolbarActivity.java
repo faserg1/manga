a3ba3d3c672497cfa597aff7d7ae7234c0f65b5d
Semyon <samvimes@yandex.ru>
2016-02-06 19:12:30 +0300

    changing fonts

--- app/src/main/java/com/danilov/supermanga/activity/BaseToolbarActivity.java
+++ app/src/main/java/com/danilov/supermanga/activity/BaseToolbarActivity.java
@@ -1,12 +1,17 @@
 package com.danilov.supermanga.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.danilov.supermanga.R;
+import com.danilov.supermanga.core.util.Utils;
 
 /**
  * Created by Semyon on 06.12.2014.
@@ -24,6 +29,14 @@ public class BaseToolbarActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setLogo(R.drawable.ic_launcher);
         setSupportActionBar(toolbar);
+
+        TextView toolbarTitleTextView = getToolbarTitleView(this, toolbar);
+        if (toolbarTitleTextView != null) {
+            Typeface typeface = Utils.getTypeface("fonts/rmedium.ttf");
+            if (typeface != null) {
+                toolbarTitleTextView.setTypeface(typeface);
+            }
+        }
     }
 
     public <T> T findViewWithId(final int id) {
@@ -50,6 +63,29 @@ public class BaseToolbarActivity extends ActionBarActivity {
     public void setContentView(final View view, final ViewGroup.LayoutParams params) {
         super.setContentView(view, params);
         setupToolbar();
+    }
+
+    public static TextView getToolbarTitleView(ActionBarActivity activity, Toolbar toolbar){
+        ActionBar actionBar = activity.getSupportActionBar();
+        CharSequence actionbarTitle = null;
+        if(actionBar != null)
+            actionbarTitle = actionBar.getTitle();
+        actionbarTitle = TextUtils.isEmpty(actionbarTitle) ? toolbar.getTitle() : actionbarTitle;
+        if(TextUtils.isEmpty(actionbarTitle)) return null;
+        // can't find if title not set
+        for(int i= 0; i < toolbar.getChildCount(); i++){
+            View v = toolbar.getChildAt(i);
+            if(v != null && v instanceof TextView){
+                TextView t = (TextView) v;
+                CharSequence title = t.getText();
+                if(!TextUtils.isEmpty(title) && actionbarTitle.equals(title) && t.getId() == View.NO_ID){
+                    //Toolbar does not assign id to views with layout params SYSTEM, hence getId() == View.NO_ID
+                    //in same manner subtitle TextView can be obtained.
+                    return t;
+                }
+            }
+        }
+        return null;
     }
 
 }
