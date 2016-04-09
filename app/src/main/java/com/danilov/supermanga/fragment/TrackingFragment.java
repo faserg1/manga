6446617363825218a461ebbdf9385a9e606e8be8
Semyon <samvimes@yandex.ru>
2016-04-09 18:59:43 +0300

    remod old icon; fixed tracking fragment (rewritten as favorites fragment)

--- app/src/main/java/com/danilov/supermanga/fragment/TrackingFragment.java
+++ app/src/main/java/com/danilov/supermanga/fragment/TrackingFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Filter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -22,6 +22,8 @@ import com.danilov.supermanga.R;
 import com.danilov.supermanga.activity.MainActivity;
 import com.danilov.supermanga.activity.MangaInfoActivity;
 import com.danilov.supermanga.core.adapter.BaseAdapter;
+import com.danilov.supermanga.core.adapter.BaseGridAdapter;
+import com.danilov.supermanga.core.adapter.ItemClickListener;
 import com.danilov.supermanga.core.application.MangaApplication;
 import com.danilov.supermanga.core.database.DatabaseAccessException;
 import com.danilov.supermanga.core.database.HistoryDAO;
@@ -42,7 +44,7 @@ import javax.inject.Inject;
 /**
  * Created by Semyon on 22.12.2014.
  */
-public class TrackingFragment extends BaseFragmentNative implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+public class TrackingFragment extends BaseFragmentNative {
 
     private static final String TAG = "TrackingFragment";
 
@@ -60,7 +62,7 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
     private int sizeOfImage;
 
     private TrackingAdapter adapter = null;
-    private GridView gridView = null;
+    private RecyclerView recyclerView = null;
     private EditText filterEditText = null;
 
     public static TrackingFragment newInstance() {
@@ -82,12 +84,10 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
         sizeOfImage = getActivity().getResources().getDimensionPixelSize(R.dimen.grid_item_height);
         mangaDAO = ServiceContainer.getService(MangaDAO.class);
         historyDAO = ServiceContainer.getService(HistoryDAO.class);
-        gridView = findViewById(R.id.grid_view);
+        recyclerView = findViewById(R.id.recycler_view);
         filterEditText = findViewById(R.id.filter);
         findViewById(R.id.clear).setOnClickListener(v -> filterEditText.setText(""));
         downloadedProgressBar = (ProgressBar) view.findViewById(R.id.downloaded_progress_bar);
-        gridView.setOnItemClickListener(this);
-        gridView.setOnItemLongClickListener(this);
         loadDownloadedManga();
         super.onActivityCreated(savedInstanceState);
     }
@@ -114,11 +114,25 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
                 final String error = _error;
                 handler.post(() -> {
                     MangaFilter filter = new MangaFilter(filterEditText, mangas);
-                    adapter = new TrackingAdapter(context, 0, mangas, filter);
-                    filter.setAdapterAccessor(new MangaFilter.AdapterAccessor(adapter));
+                    adapter = new TrackingAdapter(recyclerView, mangas, filter);
+                    adapter.setClickListener(listener);
+                    filter.setAdapterAccessor(new MangaFilter.BaseAdapterAccessor() {
+                        @Override
+                        public void notifyDataSetInvalidated() {
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void notifyDataSetChanged(final List<Manga> mangaList) {
+                            List<Manga> items = adapter.getItems();
+                            items.clear();
+                            items.addAll(mangaList);
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
                     downloadedProgressBar.setVisibility(View.INVISIBLE);
                     if (success) {
-                        gridView.setAdapter(adapter);
+                        recyclerView.setAdapter(adapter);
                     } else {
                         String formedError = Utils.stringResource(getActivity(), R.string.p_failed_to_show_loaded);
                         Utils.showToast(getActivity(), formedError + error);
@@ -131,43 +145,61 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
     }
 
 
+    private ItemClickListener<Holder> listener = new ItemClickListener<Holder>() {
 
+        @Override
+        public void onItemClick(final int position, final Holder viewHolder) {
+            Manga manga = adapter.getItem(position);
 
-    @Override
-    public void onItemClick(final AdapterView<?> adapterView, final View view, final int i, final long l) {
-        Manga manga = adapter.getItem(i);
+            Intent intent = new Intent(getActivity().getApplicationContext(), MangaInfoActivity.class);
 
-        Intent intent = new Intent(getActivity().getApplicationContext(), MangaInfoActivity.class);
+            ImageView iv = (ImageView) view.findViewById(R.id.manga_cover);
+            int[] onScreenLocation = new int[2];
+            iv.getLocationOnScreen(onScreenLocation);
 
-        ImageView iv = (ImageView) view.findViewById(R.id.manga_cover);
-        int[] onScreenLocation = new int[2];
-        iv.getLocationOnScreen(onScreenLocation);
+            intent.putExtra(MangaInfoActivity.EXTRA_LEFT, onScreenLocation[0]);
+            intent.putExtra(MangaInfoActivity.EXTRA_TOP, onScreenLocation[1]);
+            intent.putExtra(MangaInfoActivity.EXTRA_WIDTH, iv.getWidth());
+            intent.putExtra(MangaInfoActivity.EXTRA_HEIGHT, iv.getHeight());
+            intent.putExtra(MangaInfoActivity.EXTRA_HEIGHT, iv.getHeight());
 
-        intent.putExtra(MangaInfoActivity.EXTRA_LEFT, onScreenLocation[0]);
-        intent.putExtra(MangaInfoActivity.EXTRA_TOP, onScreenLocation[1]);
-        intent.putExtra(MangaInfoActivity.EXTRA_WIDTH, iv.getWidth());
-        intent.putExtra(MangaInfoActivity.EXTRA_HEIGHT, iv.getHeight());
-        intent.putExtra(MangaInfoActivity.EXTRA_HEIGHT, iv.getHeight());
+            intent.putExtra(Constants.MANGA_PARCEL_KEY, manga);
+            startActivity(intent);
 
-        intent.putExtra(Constants.MANGA_PARCEL_KEY, manga);
-        startActivity(intent);
+            getActivity().overridePendingTransition(0, 0);
+        }
 
-        getActivity().overridePendingTransition(0, 0);
-    }
+        @Override
+        public boolean onItemLongClick(final int position, final Holder viewHolder) {
+            return false;
+        }
 
-    @Override
-    public boolean onItemLongClick(final AdapterView<?> adapterView, final View view, final int i, final long l) {
-        return false;
-    }
+    };
 
-    private class TrackingAdapter extends BaseAdapter<Holder, Manga> {
+    private class TrackingAdapter extends BaseGridAdapter<Holder> {
 
         private MangaFilter filter;
+        private List<Manga> items;
 
-        public TrackingAdapter(final Context context, final int resource, final List<Manga> objects,
-                               final MangaFilter filter) {
-            super(context, resource, objects);
+        public TrackingAdapter(final RecyclerView recyclerView, final List<Manga> objects,
+                                final MangaFilter filter) {
+            super(recyclerView);
             this.filter = filter;
+            this.items = objects;
+        }
+
+        public Manga getItem(final int position) {
+            return items.get(position);
+        }
+
+        public List<Manga> getItems() {
+            return items;
+        }
+
+        @Override
+        public Holder newViewHolder(final ViewGroup parent, final int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorites_grid_item, parent, false);
+            return new Holder(v);
         }
 
         @Override
@@ -199,26 +231,13 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
         }
 
         @Override
-        public Holder onCreateViewHolder(final ViewGroup viewGroup, final int position) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.favorites_grid_item, viewGroup, false);
-            return new Holder(v);
-        }
-
-        @Override
-        public Filter getFilter() {
-            return filter;
+        public int getItemCount() {
+            return items.size();
         }
 
     }
 
-    @Override
-    public boolean onBackPressed() {
-        MainActivity activity = (MainActivity) getActivity();
-        activity.showMainFragment();
-        return true;
-    }
-
-    private static class Holder extends BaseAdapter.BaseHolder {
+    private static class Holder extends RecyclerView.ViewHolder {
 
         public TextView title;
         public View isOnline;
@@ -226,9 +245,9 @@ public class TrackingFragment extends BaseFragmentNative implements AdapterView.
 
         protected Holder(final View view) {
             super(view);
-            mangaCover = findViewById(R.id.manga_cover);
-            isOnline = findViewById(R.id.is_online);
-            title = findViewById(R.id.manga_title);
+            mangaCover = (ImageView) view.findViewById(R.id.manga_cover);
+            isOnline = view.findViewById(R.id.is_online);
+            title = (TextView) view.findViewById(R.id.manga_title);
         }
 
     }
